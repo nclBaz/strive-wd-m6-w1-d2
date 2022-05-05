2bbfbf3d89c45097da583e28c9f6e60e0d130301
Riccardo Gulin <riccardo@nucleode.com>
2022-05-05 20:36:06 +0200

    Adds middlewares & error handlers examples

--- src/server.js
+++ src/server.js
@@ -1,19 +1,48 @@
 import express from "express"
 import listEndpoints from "express-list-endpoints"
 import mongoose from "mongoose"
+import createError from "http-errors"
 import usersRouter from "./services/users/index.js"
 import booksRouter from "./services/books/index.js"
+import { badRequestHandler, unauthorizedHandler, forbiddenHandler, notFoundHandler, genericErrorHandler } from "./errorsHandlers.js"
 
 const server = express()
 
 const port = 3001
 
+// *************************** MIDDLEWARES **************************************************
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Incoming request --> ${req.method} -- ${new Date()}`)
+  // req.name = "Renan"
+  next()
+}
+
+const policeOfficerMiddleware = (req, res, next) => {
+  console.log("Current user name: ", req.name)
+  if (req.name === "Renan") {
+    next()
+  } else {
+    next(createError(401, "Only Renans allowed!"))
+  }
+}
+// ** GLOBAL LEVEL MIDDLEWARES **
+
+server.use(loggerMiddleware)
+// server.use(policeOfficerMiddleware)
 server.use(express.json()) // if you don't add this line BEFORE the endpoints, all requests' bodies will be UNDEFINED
 
 // ************************ ENDPOINTS *******************
 
-server.use("/users", usersRouter) // all the endpoints in the usersRouter they gonna have http://localhost:3001/users as a URL
+server.use("/users", usersRouter) // in this case loggerMiddleware is used as a ROUTER LEVEL MIDDLEWARE
 server.use("/books", booksRouter)
+
+// *********************** ERROR HANDLERS ********************************
+server.use(badRequestHandler)
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
 
 // ********************** DATABASE CONNECTION ************************
 
--- src/services/users/index.js
+++ src/services/users/index.js
@@ -7,48 +7,87 @@
 // 5. DELETE http://localhost:3001/users/:id --> DELETES A SINGLE USER (specified by id)
 
 import express from "express"
+import createError from "http-errors"
 import UsersModel from "./model.js"
 
 const usersRouter = express.Router()
 
+// const anotherStupidMiddleware = (req, res, next) => {
+//   console.log("I am a stupid middleware!")
+//   next()
+// }
+
 // 1.
-usersRouter.post("/", async (req, res) => {
-  // (req, res) => {} is the ENDPOINT HANDLER. Is the function that will be executed every time a request on that endpoint is sent. req and res are REQUEST and RESPONSE objects
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    // (req, res, next) => {} is the ENDPOINT HANDLER. Is the function that will be executed every time a request on that endpoint is sent. req and res are REQUEST and RESPONSE objects
 
-  console.log("REQUEST BODY: ", req.body)
+    console.log("REQUEST BODY: ", req.body)
 
-  const newUser = new UsersModel(req.body) // this is going to VALIDATE the req.body
-  const savedUser = await newUser.save() // This saves the validated body into the users' collection
+    const newUser = new UsersModel(req.body) // this is going to VALIDATE the req.body
+    const savedUser = await newUser.save() // This saves the validated body into the users' collection
 
-  res.send(savedUser)
+    res.send(savedUser)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // 2.
-usersRouter.get("/", async (req, res) => {
-  const users = await UsersModel.find()
-  res.send(users)
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    // throw new Error("KABOOOOOOOOOOOOOOOM!")
+    const users = await UsersModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // 3.
-usersRouter.get("/:userId", async (req, res) => {
-  const user = await UsersModel.findById(req.params.userId)
-  res.send(user)
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      res.send(user)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 // 4.
-usersRouter.put("/:userId", async (req, res) => {
-  const updatedUser = await UsersModel.findByIdAndUpdate(
-    req.params.userId, // WHO
-    req.body, // HOW
-    { new: true } // OPTIONS (if you want to obtain the updated user you should specify new: true)
-  )
-  res.send(updatedUser)
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId, // WHO
+      req.body, // HOW
+      { new: true } // OPTIONS (if you want to obtain the updated user you should specify new: true)
+    )
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 // 5.
-usersRouter.delete("/:userId", async (req, res) => {
-  await UsersModel.findByIdAndDelete(req.params.userId)
-  res.status(204).send()
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 export default usersRouter
